75b539e3d8463de0efa4ed32a00ef039b898f857
wxsm <wxsms@foxmail.com>
2020-08-16 11:14:33 +0800

    feat:转发时允许转发顶层页面

--- miniprogram/mixins/share.mixin.js
+++ miniprogram/mixins/share.mixin.js
@@ -3,10 +3,17 @@ import { debug } from '../utils/log.utils'
 module.exports = Behavior({
   methods: {
     onShareAppMessage () {
-      debug('onShareAppMessage')
+      let path
+      try {
+        path = getCurrentPages()[0].route
+      } catch (e) {
+        debug(e)
+        path = 'pages/tabbar/pages/index/index'
+      }
+      debug('onShareAppMessage', path)
       return {
         title: 'IBD日记',
-        path: 'pages/tabbar/pages/index/index'
+        path
       }
     }
   }

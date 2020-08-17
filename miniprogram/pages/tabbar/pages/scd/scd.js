ab3cd712d5e4fb4557bbb9852f628193d9e22d27
wxsm <wxsms@foxmail.com>
2020-08-17 23:18:06 +0800

    feat:scd食物耐受增加描述

--- miniprogram/pages/tabbar/pages/scd/scd.js
+++ miniprogram/pages/tabbar/pages/scd/scd.js
@@ -5,6 +5,8 @@ import shareMixin from '../../../../mixins/share.mixin'
 import { getFoods, SCD_LEVEL } from '../../../../store/scd-foods.store'
 import { debug, error } from '../../../../utils/log.utils'
 import find from 'lodash.find'
+import get from 'lodash.get'
+import { nextTick } from '../../../../utils/wx.utils'
 
 function initFoods () {
   debug('initFoods')
@@ -30,16 +32,30 @@ Component({
     activeTab: 0,
     selectedFood: null,
     showActionSheet: false,
+    showDesc: false,
+    descOption: null,
     options: [{
       text: '待定',
       value: -1
     }, {
       text: '耐受',
-      value: 1
+      value: 1,
+      desc: [
+        '需要煮熟',
+        '需要打糊',
+        '仅调味'
+      ]
     }, {
       text: '不耐受',
       value: 2,
-      type: 'warn'
+      type: 'warn',
+      desc: [
+        '腹泻',
+        '腹痛',
+        '便血',
+        '肠鸣',
+        '其它过敏反应'
+      ]
     }, {
       text: '没吃过',
       value: 0
@@ -119,8 +135,11 @@ Component({
               record: {
                 ...this.data.record,
                 ...newRecord
-              }
+              },
+              showActionSheet: false
             })
+            await nextTick()
+            this.showDescIfNeeded(value)
           } else {
             toast(TOAST_ERRORS.NETWORK_ERR)
           }
@@ -139,17 +158,77 @@ Component({
               record: {
                 _id,
                 ...newRecord
-              }
+              },
+              showActionSheet: false
             })
+            await nextTick()
+            this.showDescIfNeeded(value)
           } else {
             toast(TOAST_ERRORS.NETWORK_ERR)
           }
         }
-        this.hideAction()
       } catch (e) {
         error(e)
         toast(TOAST_ERRORS.NETWORK_ERR)
       } finally {
+        loading(false)
+      }
+    },
+    showDescIfNeeded (value) {
+      const valueOption = find(this.data.options, v => v.value === value)
+      const descArr = get(valueOption, 'desc')
+      if (Array.isArray(descArr) && descArr.length) {
+        this.setData({
+          showDesc: true,
+          descOption: {
+            ...valueOption,
+            desc: valueOption.desc.map(v => {
+              return {
+                text: v,
+                checked: get(this.data.record, `${this.data.selectedFood._id}.desc`, []).indexOf(v) >= 0
+              }
+            })
+          }
+        })
+      }
+    },
+    onDescChange ({ detail: { value } }) {
+      this._descArr = value
+    },
+    hideDesc () {
+      this._descArr = null
+      this.setData({ showDesc: false })
+    },
+    async saveDesc () {
+      debug(this._descArr)
+      try {
+        loading(true, '记录中...')
+        const db = wx.cloud.database()
+        const { stats: { updated } } = await db
+          .collection('records-scd')
+          .doc(this.data.record._id)
+          .update({
+            data: {
+              [`desc.${this.data.selectedFood._id}`]: this._descArr
+            }
+          })
+        if (updated === 1) {
+          loading(false)
+          this.setData({
+            record: {
+              ...this.data.record,
+              desc: {
+                ...this.data.record.desc,
+                [this.data.selectedFood._id]: this._descArr
+              }
+            },
+            showDesc: false
+          })
+        } else {
+          toast(TOAST_ERRORS.NETWORK_ERR)
+        }
+      } catch (e) {
+        error(e)
         loading(false)
       }
     },

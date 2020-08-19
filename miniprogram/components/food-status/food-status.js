95380652e97af1cba4c5f4bfbd887c622490957b
wxsm <wxsms@foxmail.com>
2020-08-19 19:02:20 +0800

    refactor:scd状态标志组件重构

---
+++ miniprogram/components/food-status/food-status.js
@@ -0,0 +1,42 @@
+import isNil from 'lodash.isnil'
+import get from 'lodash.get'
+import isNumber from 'lodash.isnumber'
+import find from 'lodash.find'
+import { SCD_STATUS } from '../../constants/constants'
+import computedBehavior from 'miniprogram-computed'
+
+Component({
+  behaviors: [computedBehavior],
+  properties: {
+    record: null,
+    showText: {
+      type: Boolean,
+      value: false
+    }
+  },
+  data: {
+    SCD_STATUS
+  },
+  computed: {
+    recordStatus ({ record }) {
+      if (isNil(record)) {
+        return ''
+      }
+      let status = record
+      if (!isNumber(status)) {
+        status = get(status, 'status')
+      }
+      return status
+    },
+    recordStatusText ({ record }) {
+      if (isNil(record)) {
+        return ''
+      }
+      let status = record
+      if (!isNumber(status)) {
+        status = get(status, 'status')
+      }
+      return get(find(SCD_STATUS, v => v.value === status), 'text', SCD_STATUS.NOT_EAT.text)
+    }
+  }
+})
--- miniprogram/pages/modules/scd/pages/detail/detail.js
+++ miniprogram/pages/modules/scd/pages/detail/detail.js
@@ -1,19 +1,13 @@
 import { storeBindingsBehavior } from 'mobx-miniprogram-bindings'
-import computedBehavior from 'miniprogram-computed'
 import themeMixin from '../../../../../mixins/theme.mixin'
 import shareMixin from '../../../../../mixins/share.mixin'
 import { debug } from '../../../../../utils/log.utils'
 import { loading, toast, TOAST_ERRORS } from '../../../../../utils/toast.utils'
 import { removeLevelInName } from '../../../../../store/scd-foods.store'
 import { store } from '../../../../../store/store'
-import isNil from 'lodash.isnil'
-import find from 'lodash.find'
-import get from 'lodash.get'
-import isNumber from 'lodash.isnumber'
-import { SCD_STATUS } from '../../../../../constants/constants'
 
 Component({
-  behaviors: [storeBindingsBehavior, computedBehavior, themeMixin, shareMixin],
+  behaviors: [storeBindingsBehavior, themeMixin, shareMixin],
   data: {
     foodData: null
   },
@@ -24,28 +18,6 @@ Component({
     },
     actions: {
       setScdRecord: 'setScdRecord'
-    }
-  },
-  computed: {
-    recordStatus ({ record, foodData }) {
-      if (isNil(record) || isNil(foodData)) {
-        return ''
-      }
-      let status = get(record, foodData._id)
-      if (!isNumber(status)) {
-        status = get(status, 'status')
-      }
-      return status
-    },
-    recordStatusText ({ record, foodData }) {
-      if (isNil(record) || isNil(foodData)) {
-        return ''
-      }
-      let status = get(record, foodData._id)
-      if (!isNumber(status)) {
-        status = get(status, 'status')
-      }
-      return get(find(SCD_STATUS, v => v.value === status), 'text', SCD_STATUS.NOT_EAT.text)
     }
   },
   methods: {

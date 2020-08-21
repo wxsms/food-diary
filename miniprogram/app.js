827ff3c2b6bcc891f5d3ffac71615cdb68d51f40
wxsm <wxsms@foxmail.com>
2020-08-21 12:56:12 +0800

    feat:更新提示

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,3 +1,5 @@
+import { debug } from './utils/log.utils'
+
 App({
   onLaunch () {
     Promise.prototype.finally = function (callback) {
@@ -13,5 +15,32 @@ App({
       traceUser: true
     })
     this.globalData = {}
+
+    // 更新检测
+    const updateManager = wx.getUpdateManager()
+
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      debug('onCheckForUpdate', res)
+    })
+
+    updateManager.onUpdateReady(function () {
+      debug('onUpdateReady')
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已准备好，是否重启？',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+
+    updateManager.onUpdateFailed(function () {
+      // 新版本下载失败
+      debug('onUpdateFailed')
+    })
   }
 })
